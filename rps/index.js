e201158af6797de4ac05b495d600fafbebd78893
semi <semilore@omotade-michaels.com>
2022-12-29 18:50:06 +0100

    rps basic game js for 1 round

---
+++ rps/index.js
@@ -0,0 +1,56 @@
+// get id of display
+
+const computerChoiceDisplay = document.getElementById("computer-choice");
+const userChoiceDisplay = document.getElementById("user-choice");
+const resultDisplay = document.getElementById("result");
+
+//select all buttons
+const possibleChoices = document.querySelectorAll("button");
+let userChoice;
+//for each button selected, set display to value of btn content
+possibleChoices.forEach((possibleChoice) =>
+  possibleChoice.addEventListener("click", (e) => {
+    userChoice = e.target.id;
+    userChoiceDisplay.innerHTML = userChoice;
+    generateComputerChoice();
+    getResult();
+  })
+);
+
+const choices = ["rock", "paper", "scissors"];
+let computerChoice;
+let result;
+// generate computer choice and display to DOM
+function generateComputerChoice() {
+  const randomNumber = Math.floor(Math.random() * choices.length);
+  computerChoice = choices[randomNumber];
+
+  computerChoiceDisplay.innerHTML = computerChoice;
+}
+
+function getResult() {
+  if (computerChoice === userChoice) {
+    result = "its a draw";
+  }
+
+  if (computerChoice === "rock" && userChoice === "paper") {
+    result = "you win!";
+  }
+  if (computerChoice === "scissors" && userChoice === "paper") {
+    result = "you lose!";
+  }
+  if (computerChoice === "rock" && userChoice === "scissors") {
+    result = "you lose!";
+  }
+  if (computerChoice === "paper" && userChoice === "scissors") {
+    result = "you win!";
+  }
+  if (computerChoice === "scissors" && userChoice === "rock") {
+    result = "you win!";
+  }
+  if (computerChoice === "paper" && userChoice === "rock") {
+    result = "you lose!";
+  }
+
+  resultDisplay.innerHTML = result;
+}
